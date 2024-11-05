1077ba7fc9d4f28990ed77a1549303787a0ba956
jhonPedroo <132566203+jhonPedroo@users.noreply.github.com>
2024-11-05 08:07:46 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,55 @@
+criaCartao(
+    'volei',
+    'O que é o vôlei?',
+    'O vôlei é um esporte coletivo em que duas equipes tentam marcar pontos, passando uma bola por cima de uma rede, com a intenção de fazê-la tocar no chão do lado adversário. Cada equipe pode tocar a bola até três vezes antes de enviá-la ao campo contrário'
+    
+    )
+
+criaCartao(
+    'volei',
+    'Quantos jogadores compõem uma equipe de vôlei?',
+    'Cada equipe de vôlei é composta por seis jogadores em quadra. No total, uma equipe pode ter mais jogadores em seu elenco, mas apenas seis estão em jogo a cada momento.'
+)
+
+criaCartao(
+    'volei',
+    'Qual a altura da rede no vôlei masculino e feminino?',
+    'A altura da rede para o vôlei masculino é de 2,43 metros, enquanto para o feminino é de 2,24 metros.'
+)
+
+criaCartao(
+    'volei',
+    'O que é um "ace" no vôlei?',
+    'Um "ace" ocorre quando o jogador realiza um saque que não é tocado ou defendido pelo time adversário, resultando diretamente em ponto para sua equipe.'
+)
+
+criaCartao(
+    'volei',
+    'Quantos sets são jogados em uma partida de vôlei?',
+    'Uma partida de vôlei geralmente é jogada em melhor de cinco sets. O time que vencer três sets primeiro, vence a partida. Cada set vai até 25 pontos, mas é necessário ter uma vantagem de pelo menos 2 pontos. Se o jogo for para o quinto set, ele vai até 15 pontos.'
+)
+criaCartao(
+    'volei',
+    'O que é o "O que é o "levantamento" no vôlei?',
+    'O levantamento é uma técnica utilizada para passar a bola com precisão para um companheiro de equipe, geralmente para que ele faça um ataque. O levantador usa as mãos para lançar a bola com efeito e precisão.'
+)
+criaCartao(
+    'volei',
+    'O que é o "bloqueio" no vôlei?',
+    'O bloqueio é uma ação defensiva em que um ou mais jogadores da equipe saltam perto da rede para interceptar ou dificultar o ataque do time adversário, tentando impedir que a bola passe por cima da rede.'
+)
+criaCartao(
+    'volei',
+    'O que é o "Quantos toques uma equipe pode dar na bola antes de passá-la para o adversário?',
+    'Cada equipe pode dar até três toques na bola antes de passá-la para o adversário. O primeiro toque geralmente é o "recepção" ou "passagem", o segundo é o "levantamento" e o terceiro é o "ataque" ou "finalização".'
+)
+criaCartao(
+    'volei',
+    'Qual é o papel do líbero no vôlei?',
+    'O líbero é um jogador especializado na recepção de saques e no levantamento defensivo. Ele não pode atacar a bola acima da rede ou fazer saques, mas é fundamental para melhorar a defesa da equipe.'
+)
+criaCartao(
+    'volei',
+    'O que caracteriza um "defesa" no vôlei?',
+    'A defesa é a ação em que os jogadores tentam evitar que a bola do adversário toque no chão de seu lado da quadra. Isso pode incluir passes, recepções e bloqueios, além de técnicas como a "manchete" ou "pancada".'
+)
